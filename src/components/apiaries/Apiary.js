059076bd9ab48355650f824e7f6b392abe828952
Brant2001 <brant.pippin@gmail.com>
2020-05-04 14:46:41 -0500

    Added a delete button to each apiary in the apiary list.

--- src/components/apiaries/Apiary.js
+++ src/components/apiaries/Apiary.js
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Button } from "reactstrap"
+import { ApiaryContext } from "./ApiaryProvider"
 
 // When we use Apiary component in ApiaryList, React takes the keys passed
 // to the Apiary component and puts it into one object
-export const Apiary = ({apiary}) => (
-    <section className="apiary">
-        <h3 className="apiary__name">{apiary.name}</h3>
-        <div className="apiary__loc">Location: {apiary.location}</div>
-    </section>
-)
+
+export const Apiary = ({apiary}) => {
+    const { deleteApiary } = useContext(ApiaryContext)
+
+    return (
+        <section className="apiary">
+            <h3 className="apiary__name">{apiary.name}</h3>
+            <div className="apiary__loc">Location: {apiary.location}</div>
+            <Button color="danger" onClick={() => {
+                deleteApiary(apiary.id)
+            }}>Delete</Button>
+        </section>
+    )
+}

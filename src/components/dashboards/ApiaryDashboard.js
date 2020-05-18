9e2d67ecd52f59f4d0c6c1037944d2653ae9b2d3
Brant2001 <brant.pippin@gmail.com>
2020-05-18 10:36:18 -0500

    Added a back btn and a logout btn.

--- src/components/dashboards/ApiaryDashboard.js
+++ src/components/dashboards/ApiaryDashboard.js
@@ -3,10 +3,17 @@ import { ApiaryProvider } from "../apiaries/ApiaryProvider"
 import { ApiaryList } from "../apiaries/ApiaryList"
 import { UserProvider } from "../users/UserProvider"
 import "../App.css"
+import { Button } from "reactstrap"
 
 export const ApiaryDashboard = ( { setActiveList} ) => {
     return (    
             <div className="apiaryContainer">
+                <Button
+                onClick={() => {
+                localStorage.removeItem("hiveLogger_user")
+                window.location.reload()
+                }}>Buzz Off</Button>
+                
                 <h1>HiveLogger</h1>
                 <small>An app worth buzzing about.</small>
                     <UserProvider>
